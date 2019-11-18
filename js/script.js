954d188296ac20055c2a81394912b8252cd45e04
Foolings <m.bolden2001@gmail.com>
2019-11-18 23:05:39 +0000

    11/18/19

--- js/script.js
+++ js/script.js
@@ -8,4 +8,19 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    var computerChoice = 7
+    $("#computerChoice").append(computerChoice);
+     var userChoice = $(input).val();
+    $("#userChoice").append(userChoice);
+    if (userChoice == 3 && computerChoice == 7) {
+         $("#result").append("Lose");
+    } 
+});
+ // var normal = 3;
+ // var fighting  = 7;
+// var flying = 8;
+// var poison = 2; 
+// var ice = 9;
+
 
