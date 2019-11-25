c6c8dd32b8d8a28f8ce1fa34533626b014d0781e
Foolings <m.bolden2001@gmail.com>
2019-11-25 02:56:15 +0000

    11/24/19

--- js/script.js
+++ js/script.js
@@ -175,8 +175,332 @@ $("#shoot").click(function(){
         $("#result").append("Win");
     } else if (userChoice == 9 && computerChoice == 19) {
         $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 2) {
+        $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 3) {
+        $("#result").append("Lose");
+    } else if (userChoice == 1 && computerChoice == 4) { 
+        $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 5) {
+        $("#result").append("Lose");
+    } else if (userChoice == 1 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 1 && computerChoice == 8) {
+        $("#result").append("Win");
+    } else if (userChoice == 1 && computerChoice == 9) {
+        $("#result").append("Lose");
+    } else if (userChoice == 2 && computerChoice == 1) {
+        $("#result").append("Lose");
+    } else if (userChoice == 2 && computerChoice == 2) {
+        $("#result").append("Win");
+    } else if (userChoice == 2 && computerChoice == 3) {
+        $("#result").append("Win");
+    } else if (userChoice == 2 && computerChoice == 4) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 2 && computerChoice == 5) {
+        $("#result").append("Lose");
+    } else if (userChoice == 2 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 2 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 2 && computerChoice == 8) {
+        $("#result").append("Win");
+    } else if (userChoice == 2 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 3 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 3 && computerChoice == 2) {
+        $("#result").append("Win");
+    } else if (userChoice == 3 && computerChoice == 3) {
+        $("#result").append("Lose");
+    } else if (userChoice == 3 && computerChoice == 4) {
+        $("#result").append("Lose");
+    } else if (userChoice == 3 && computerChoice == 5) {
+        $("#result").append("Lose");
+    } else if (userChoice == 3 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 3 && computerChoice == 7) {
+        $("#result").append("Lose")
+    } else if (userChoice == 3 && computerChoice == 8) {
+        $("#result").append("Win");
+    } else if (userChoice == 3 && computerChoice == 9) {
+        $("#result").append("Lose");
+    } else if (userChoice == 4 && computerChoice == 1) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 4 && computerChoice == 2) {
+        $("#result").append("Lose");
+    } else if (userChoice == 4 && computerChoice == 3) {
+        $("#result").append("Lose");
+    } else if (userChoice == 4 && computerChoice == 4) {
+        $("#result").append("Lose"); 
+    } else if (userChoice == 4 && computerChoice == 5) {
+        $("#result").append("Lose"); 
+    } else if (userChoice == 4 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 4 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 4 && computerChoice == 8) {
+        $("#result").append("Lose");
+    } else if (userChoice == 4 && computerChoice == 9) {
+        $("#result").append("Lose");
+    } else if (userChoice == 5 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 5 && computerChoice == 2) {
+        $("#result").append("Lose");
+    } else if (userChoice == 5 && computerChoice == 3) {
+        $("#result").append("Win");
+    } else if (userChoice == 5 && computerChoice == 4) {
+        $("#result").append("Win");
+    } else if (userChoice == 5 && computerChoice == 5) {
+        $("#result").append("Lose"); 
+    } else if (userChoice == 5 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 5 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 5 && computerChoice == 8) {
+        $("#result").append("Lose");
+    } else if (userChoice == 5 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 6 && computerChoice == 1) {
+        $("#result").append("Lose");
+    } else if (userChoice == 6 && computerChoice == 2) {
+        $("#result").append("Lose");
+    } else if (userChoice == 6 && computerChoice == 3) {
+        $("#result").append("Win");
+    } else if (userChoice == 6 && computerChoice == 4) {
+        $("#result").append("Win");
+    } else if (userChoice == 6 && computerChoice == 5) {
+        $("#result").append("Win");
+    } else if (userChoice == 6 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 6 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 6 && computerChoice == 8) {
+        $("#result").append("Lose");
+    } else if (userChoice == 6 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 2) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 3) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 4) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 5) {
+        $("#result").append("Win")
+    } else if (userChoice == 7 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 7 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 7 && computerChoice == 8) {
+        $("#result").append("Lose");
+    } else if (userChoice == 7 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 2) {
+        $("#result").append("Lose");
+    } else if (userChoice == 8 && computerChoice == 3) {
+        $("#result").append("Lose");
+    } else if (userChoice == 8 && computerChoice == 4) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 5) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 8 && computerChoice == 8) {
+        $("#result").append("Win");
+    } else if (userChoice == 8 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 1) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 2) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 3) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 9 && computerChoice == 4) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 5) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 6) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 7) {
+        $("#result").append("Lose");
+    } else if (userChoice == 9 && computerChoice == 8) {
+        $("#result").append("Win");
+    } else if (userChoice == 9 && computerChoice == 9) {
+        $("#result").append("Win");
+    } else if (userChoice == 11 && computerChoice == 11) {
+        $("#result").append("Lose");
+    } else if (userChoice == 11 && computerChoice == 12) {
+        $("#result").append("Win");
+    } else if (userChoice == 11 && computerChoice == 13) {
+        $("#result").append("Win");
+    } else if (userChoice == 11 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 11 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 11 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 11 && computerChoice == 17) {
+        $("#result").append("Win");
+    } else if (userChoice == 11 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 11 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 12 && computerChoice == 11) {
+        $("#result").append("Lose");
+    } else if (userChoice == 12 && computerChoice == 12) {
+        $("#result").append("Win");
+    } else if (userChoice == 12 && computerChoice == 13) {
+        $("#result").append("Lose");
+    } else if (userChoice == 12 && computerChoice == 14) {
+        $("#result").append("Win");
+    } else if (userChoice == 12 && computerChoice == 15) {
+        $("#result").append("Lose");
+    } else if (userChoice == 12 && computerChoice == 16) {
+        $("#result").append("Win");
+    } else if (userChoice == 12 && computerChoice == 17) {
+        $("#result").append("Lose");
+    } else if (userChoice == 12 && computerChoice == 18) {
+        $("#result").append("Win");
+    } else if (userChoice == 12 && computerChoice == 19) {
+        $("#result").append("Lose");
+    } else if (userChoice == 13 && computerChoice == 11) {
+        $("#result").append("Win");
+    } else if (userChoice == 13 && computerChoice == 12) {
+        $("#result").append("Lose");
+    } else if (userChoice == 13 && computerChoice == 13) {
+        $("#result").append("Win");
+    } else if (userChoice == 13 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 13 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 13 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 13 && computerChoice == 17) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 13 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 13 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 14 && computerChoice == 11) {
+        $("#result").append("Win");
+    } else if (userChoice == 14 && computerChoice == 12) {
+        $("#result").append("Lose");
+    } else if (userChoice == 14 && computerChoice == 13) {
+        $("#result").append("Win");
+    } else if (userChoice == 14 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 14 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 14 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 14 && computerChoice == 17) {
+        $("#result").append("Win");
+    } else if (userChoice == 14 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 14 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 15 && computerChoice == 11) {
+        $("#result").append("Lose");
+    } else if (userChoice == 15 && computerChoice == 12) {
+        $("#result").append("Win");
+    } else if (userChoice == 15 && computerChoice == 13) {
+        $("#result").append("Lose");
+    } else if (userChoice == 15 && computerChoice == 14) {
+        $("#result").append("Win");
+    } else if (userChoice == 15 && computerChoice == 15) {
+        $("#result").append("Lose");
+    } else if (userChoice == 15 && computerChoice == 16) {
+        $("#result").append("Win");
+    } else if (userChoice == 15 && computerChoice == 17) {
+        $("#result").append("Lose");
+    } else if (userChoice == 15 && computerChoice == 18) {
+        $("#result").append("Win");
+    } else if (userChoice == 15 && computerChoice == 19) {
+        $("#result").append("Lose");
+    } else if (userChoice == 16 && computerChoice == 11) {
+        $("#result").append("Win");
+    } else if (userChoice == 16 && computerChoice == 12) {
+        $("#result").append("Lose");
+    } else if (userChoice == 16 && computerChoice == 13) {
+        $("#result").append("Win");
+    } else if (userChoice == 16 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 16 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 16 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 16 && computerChoice == 17) {
+        $("#result").append("Win");
+    } else if (userChoice == 16 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 16 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 17 && computerChoice == 11) {
+        $("#reslt").append("Lose");
+    } else if (userChoice == 17 && computerChoice == 12) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 17 && computerChoice == 13) {
+        $("#result").append("Lose");
+    } else if (userChoice == 17 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 17 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 17 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 17 && computerChoice == 17) {
+        $("#result").append("Win");
+    } else if (userChoice == 17 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 17 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 18 && computerChoice == 11) {
+        $("#result").append("Win");
+    } else if (userChoice == 18 && computerChoice == 12) {
+        $("#result").append("Lose");
+    } else if (userChoice == 18 && computerChoice == 13) {
+        $("#result").append("Win"); 
+    } else if (userChoice == 18 && computerChoice == 14) {
+        $("#result").append("Lose");
+    } else if (userChoice == 18 && computerChoice == 15) {
+        $("#result").append("Win");
+    } else if (userChoice == 18 && computerChoice == 16) {
+        $("#result").append("Lose");
+    } else if (userChoice == 18 && computerChoice == 17) {
+        $("#result").append("Win");
+    } else if (userChoice == 18 && computerChoice == 18) {
+        $("#result").append("Lose");
+    } else if (userChoice == 18 && computerChoice == 19) {
+        $("#result").append("Win");
+    } else if (userChoice == 19 && computerChoice == 11) {
+        $("#result").append("Lose");
+    } else if (userChoice == 19 && computerChoice == 12) {
+        $("#result").append("Win");
+    } else if (userChoice == 19 && computerChoice == 13) {
+        $("#result").append("Lose");
+    } else if (userChoice == 19 && computerChoice == 14) {
+        $("#result").append("Win");
+    } else if (userChoice == 19 && computerChoice == 15) {
+        $("#result").append("Lose");
+    } else if (userChoice == 19 && computerChoice == 16) {
+        $("#result").append("Win");
+    } else if (userChoice == 19 && computerChoice == 17) {
+        $("#result").append("Lose");
+    } else if (userChoice == 19 && computerChoice == 18) {
+        $("#result").append("Win");
+    } else if (userChoice == 19 && computerChoice == 19) {
+        $("#result").append("Lose");
     }
-});
+}); 
 // var 1 = bug;
 // var 2 = electric;
 // var 3 = fire;
